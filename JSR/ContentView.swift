07de4becdf720588ed9b407e010d51f99acd4cb5
Divy Srivastava <dj.srivastava23@gmail.com>
2024-06-29 10:50:28 +0530

    Initial Commit

---
+++ JSR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JSR
+//
+//  Created by Divy Srivastava on 29/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JSR/JSRApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSRApp.swift
+//  JSR
+//
+//  Created by Divy Srivastava on 29/06/24.
+//
+
+import SwiftUI
+
+@main
+struct JSRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
